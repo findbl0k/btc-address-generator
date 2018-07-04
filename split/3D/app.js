7e6e6702e5b58a4e50d03afd2f22bdbffcfe2f0e
findbl0k <findbl0k@gmx.com>
2018-07-04 10:57:09 -0700

    formatting

--- split/3D/app.js
+++ split/3D/app.js
@@ -1,23 +1,17 @@
 var link = document.createElement( 'a' );
 	link.style.display = 'none';
-	document.body.appendChild( link ); // Firefox workaround, see #6594
+	document.body.appendChild( link ); // Firefox workaround for .stl file saving
 
-function save( blob, filename ) {
+function save( blob, filename ) { // for .stl file saving
 
-		link.href = URL.createObjectURL( blob );
-		link.download = filename || 'data.json';
-		link.click();
+	link.href = URL.createObjectURL( blob );
+	link.download = filename || 'data.json';
+	link.click();
+}
 
-		// URL.revokeObjectURL( url ); breaks Firefox...
-
-	}
-
-
-function saveString( text, filename ) {
-
-		save( new Blob( [ text ], { type: 'text/plain' } ), filename );
-
-	}
+function saveString( text, filename ) { // for .stl file saving
+	save( new Blob( [ text ], { type: 'text/plain' } ), filename );
+}
 
 function add_qr_to_scene(result){
     console.log(result);
